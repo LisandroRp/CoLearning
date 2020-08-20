5b0410c4050d6e459d97ecc4f1ab31405c804eec
Alejandro Foglino <alefoglino@gmail.com>
2020-08-20 14:25:12 -0300

    [Colearnig] - recurso get por mail

--- backend/api-routes.js
+++ backend/api-routes.js
@@ -2,7 +2,6 @@
 let router = require('express').Router();
 let usuarioController = require('./controller/UsuarioController');
 let comentarioController = require('./controller/ComentarioController');
-let peliculaController = require('./controller/PeliculaController');
 let genericoController = require('./controller/ControlerGenerico');
 let direccionController = require('./controller/DireccionController');
        
@@ -123,6 +122,15 @@ router.get('/foros/:name', (req, res) => {
         genericoController.getForoByTagsByName(req,res);
 });
 
+router.get('/tags/foros', (req, res) => {
+    console.log("Consultar foros por nombre: ", req.query.tag);
+    if(!req.query.tag || req.query.tag =='undefined' || req.query.tag == '') 
+        res.status(409).send({ msg: "El campo id nombre es requerido." });
+    else
+        console.log(req.query.tag);
+        res.sendStatus(200);
+});
+
 router.get('/institutos/:name', (req, res) => {
     console.log("Consultar institutos por nombre: ", req.params.name);
     if(!req.params.name || req.params.name =='undefined' || req.params.name == '') 
@@ -147,6 +155,21 @@ router.get('/usuarios/mail', (req, res) => {
         usuarioController.getUsuarioByMail(req,res);
 });
 
+router.get('/usuarios/mails/:mail', (req, res) => {
+    if(!req.params.mail || req.params.mail =='undefined' || req.params.mail == '') 
+        res.status(409).send({ msg: "El campo id mail es requerido." });
+    else
+        usuarioController.getUsuarioByNameMail(req,res);
+});
+
+router.get('/usuarios/name/:name', (req, res) => {
+    if(!req.params.name || req.params.name =='undefined' || req.params.name == '') 
+        res.status(409).send({ msg: "El campo id mail es requerido." });
+    else
+        usuarioController.getUsuarioByName(req,res);
+});
+
+
 //************************** Fin Recursos Usuario**************************** */
 
 //************************** Inicio Recursos Direccion**************************** */
@@ -163,135 +186,6 @@ router.get('/direcciones',(req, res) =>{
 });
 
 //************************** Fin Recursos Direccion**************************** */
-
-router.get('/getUsuarios', (req, res) => {
-    usuarioController.getUsuarios(req,res);
-});
-
-
-router.get('/Usuarios', (req, res) => {
-    usuarioController.getUsuarios(req,res);
-});
-//EndPoint para leer usuario por id
-router.get('/getUsuarioById',(req, res) =>{
-    if(!req.query.usuarioId || req.query.usuarioId =='undefined' || req.query.usuarioId == '') 
-        res.status(409).send({ msg: "El campo id usuario es requerido." });
-    else
-        usuarioController.getUsuarioById(req,res);
-});
-//Homologo al anterior
-router.get('/Usuarios/Usuario',(req, res) =>{
-    if(!req.query.usuarioId || req.query.usuarioId =='undefined' || req.query.usuarioId == '') 
-        res.status(409).send({ msg: "El campo id usuario es requerido." });
-    else
-        usuarioController.getUsuarioById(req,res);
-});
-router.get('/getUsuarioByIdOne',(req, res) =>{
-    if(!req.query.usuarioId || req.query.usuarioId =='undefined' || req.query.usuarioId == '') 
-        res.status(409).send({ msg: "El campo id usuario es requerido." });
-    else
-        usuarioController.getUsuarioByIdOne(req,res);
-});
-//Endpoint de insert un usuarios
-router.post('/insertUsuario/Usuario',(req, res) =>{
-    console.log("Insertar nuevo usuario: ", req.body);
-    if(!req.body || req.body.usuarioId =='undefined' || req.body.password =='undefined' || req.body.usuarioId == '' || req.body.password == '') 
-        res.status(409).send({ msg: "El campo id usuario es requerido." });
-    else
-        usuarioController.insertUsuario(req,res);
-});
-router.post('/Usuarios/Usuario',(req, res) =>{
-    console.log("Insertar nuevo usuario: ", req.body);
-    if(!req.body || req.body.usuarioId =='undefined' || req.body.password =='undefined' || req.body.usuarioId == '' || req.body.password == '') 
-        res.status(409).send({ msg: "El campo id usuario es requerido." });
-    else
-        usuarioController.insertUsuario(req,res);
-});
-//EndPoint para actualizar password por usuario.
-router.post('/updateUsuarioByPassword/Usuario',(req, res) =>{
-    console.log("Actualizar usuario y password: ", req.body);
-    if(!req.body.usuarioId || !req.body.password) 
-        res.status(409).send({ msg: "El campo nombre y password son requeridos del usuario." });
-    else
-        usuarioController.updateUsuarioByPassword(req,res); 
-});
-
-
-//**************************Recursos de Usuarios**************************** */
-//EndPoint para insertar comentario
-router.post('/insertComentario/Comentario',(req, res) =>{
-    console.log("Insertar nuevo comentatrio: ",req.body);
-    if(!req.body || req.body.usuarioId =='undefined' || req.body.peliculaId =='undefined' || req.body.usuarioId == '' || req.body.usuarioId == null || req.body.peliculaId == '' || req.body.peliculaId == null) 
-        res.status(409).send({ msg: "El campo usuario y pelicula son requeridos del comentario." });
-    else
-        comentarioController.insertComentario(req,res);
-});
-//EndPoint para leer comentartios de un usuarios
-router.get('/getComentariosByUsuario',(req, res) =>{
-    console.log("Obtener  comentatrio por usuario: ",req.query);
-    if(!req.query || req.query.usuarioId =='undefined' || req.query.usuarioId == '' || req.query.usuarioId == null ) 
-        res.status(409).send({ msg: "El campo usuario es requerido del comentario." });
-    else
-        comentarioController.getComentariosByUsuarioId(req,res);
-});
-//EndPoint para leer comentartios de un usuarios
-router.get('/getComentariosByPeliculaId',(req, res) =>{   
-    console.log("Obtener  comentatrio por pelicula: ",req.query);
-    if(!req.query ||  req.query.peliculaId =='undefined' || req.query.peliculaId == '' || req.query.peliculaId == null) 
-        res.status(409).send({ msg: "El campo pelicula es requeridos del comentario." });
-    else
-        comentarioController.getComentariosByPeliculaId(req,res);
-});
-//**************************Recursos de Peliculas**************************** */
-//EndPoint para buscar pelicula por titulo.
-router.get('/getPeliculasByTitle',(req, res) =>{
-    console.log("Obtener  pelicula por titulo: ",req.query);
-    if(!req.query ||  req.query.title =='undefined' || req.query.title == '' || req.query.title == null) 
-        res.status(409).send({ msg: "El campo titulo  de pelicula es requeridos." });
-    else
-        peliculaController.getPeliculasByTitle(req,res);
-});
-//EndPoint para buscar pelicula por titulo y anio.
-router.get('/getPeliculasByTitleAndYear',(req, res) =>{
-    console.log('Param getPeliculasByTitleAndYear: ',req.query);
-    if(typeof req.query.title !== 'undefined' && typeof req.query.year !== 'undefined') {
-        console.log(req.query.title);
-        console.log(req.query.year);
-        peliculaController.getPeliculasByTitleAndYear(req,res);
-    }else{
-        res.status(409).send({ msg: "Parametros invalidos." });
-    }    
-});
-//EndPoint para buscar pelicula por key de nombre de pelicula.
-router.get('/getPeliculasByKey',(req, res) =>{
-    console.log('Param getPeliculasByKey: ',req.query);
-    if(typeof req.query.key !== 'undefined') {
-        console.log(req.query.key);
-        peliculaController.getPeliculasByKey(req,res);
-    }else{
-        res.status(410).send({ msg: "Parametros no ingresado o invalidos." });
-    }    
-});
-//EndPoint para buscar pelicula por Id  de pelicula.
-router.get('/getPeliculasAndSeriesById',(req, res) =>{
-    console.log('Param getPeliculasAndSeriesById: ',req.query);
-    if(typeof req.query.movieId !== 'undefined') {
-        console.log(req.query.movieId);
-        peliculaController.getPeliculasAndSeriesById(req,res);
-    }else{
-        res.status(410).send({ msg: "Parametros no ingresado o invalidos." });
-    }    
-});
-//EndPoint para buscar series por key de nombre de pelicula.
-router.get('/getSeriesByKey',(req, res) =>{
-    console.log('Param getSeriesByKey: ',req.query);
-    if(typeof req.query.key !== 'undefined') {
-        console.log(req.query.key);
-        peliculaController.getSeriesByKey(req,res);
-    }else{
-        res.status(410).send({ msg: "Parametros no ingresado o invalidos." });
-    }    
-});
 
 // Export API routes
 module.exports = router;
--- backend/controller/UsuarioController.js
+++ backend/controller/UsuarioController.js
@@ -1,12 +1,27 @@
-var usuarios = require('../model/Usuario');
 var clases = require('../model/Clases');
+var comentarios = require('../model/Comentario')
 var cursoPorAlumnos = require('../model/CursoPorAlumno');
 var cursos = require('../model/Cursos');
+var cursosPorDondeDaClases = require('../model/CursosPorDondeDaClases');
+var cursosPorMaterias = require('../model/CursosPorMaterias');
+var cursosPorTipoClases = require('../model/CursosPorTipoClases');
 var direcciones = require('../model/Direccion');
+var dondeDaClases= require('../model/DondeDaClases');
+var dondeDaClasesPorProfesor= require('../model/DondeDaClasesPorProfesor');
 var foros = require('../model/Foro');
+var foroPorTag= require('../model/ForoPorTag');
+var instituoPorProfesores = require('../model/InstituoPorProfesores');
+var institutos = require('../model/Instituto');
+var materias= require('../model/Materia');
+var materiaProfesor = require('../model/MateriaPorProfesor');
+var claseProfesor = require('../model/ProfesorPorClase');
 var ratings = require('../model/Rating');
+var tag= require('../model/Tag');
+var tipoClase = require('../model/TipoClase');
+var usuarios = require('../model/Usuario');
 
 var bodyParser = require('body-parser');
+
 
  
 let insertUsuario = (req,res) =>
@@ -127,4 +142,270 @@ let updateUsuarioByPassword = (req, res) =>
       res.status(206).send({ msg: "Se actualizaron los usuarios." });  
 };
 
-module.exports = {getUsuarios,getUsuarioById,insertUsuario,updateUsuarioByPassword,getUsuarioByIdOne,getUsuarioByMail};
+
+
+let getUsuarioByMail1 = (req, res) =>
+{      
+    //Listar resultados
+    var respuesta;
+    console.log("llegue a leer getUsuarioByMail1 "  + req.query.mail);
+    let idBusqueda = {mail: req.query.mail};
+    var user;
+    usuarios.findOne(idBusqueda)
+    .then
+    (
+        (usuario)=>
+        {
+            console.log(usuario); 
+            if(usuario != null ){
+                var respuesta = getUsuario(usuario.nombre_usuario);
+                console.log("NO llego:" + respuesta);
+                res.status(200).send(respuesta);
+            }
+               
+        },
+        (err)=>{console.log(err);}
+    )  
+   
+};
+
+function getUsuario (name) 
+{      
+    //Listar resultados
+    console.log("llegue a leer getUsuario "  + name );
+    usuarios.aggregate([
+        {$match: { 'nombre_usuario':   name }},
+        {$lookup: 
+            {
+                from: 'direccion',
+                localField: 'idDireccion',
+                foreignField: 'idDireccion',
+                as: 'domicilio'
+            }
+        } ,
+        {$lookup: 
+            {
+                from: 'materiaProfesor',
+                localField: 'idUsuario',
+                foreignField: 'idProfesor',
+                as: 'materiaProfesores'
+            }
+        } , {$unwind: {
+            path: "$materiaProfesores",
+            preserveNullAndEmptyArrays: false
+        }}
+        ,{$lookup: 
+            {
+                from: 'materia',
+                localField: 'materiaProfesores.idMateria_FK',
+                foreignField: 'id_materia',
+                as: 'materias'
+            }
+        },
+        {$lookup: 
+            {
+                from: 'profesorPorClase',
+                localField: 'idUsuario',
+                foreignField: 'idProfesor',
+                as: 'clasesProfesores'
+            }
+        } , {$unwind: {
+            path: "$clasesProfesores",
+            preserveNullAndEmptyArrays: false
+        }},
+        {$lookup: 
+            {
+                from: 'tipoClase',
+                localField: 'clasesProfesores.idClase_FK',
+                foreignField: 'id_tipoClases',
+                as: 'tipoClases'
+            }
+        },
+        {$lookup: 
+            {
+                from: 'dondeDaClasesPorProfesor',
+                localField: 'idUsuario',
+                foreignField: 'idProfesor',
+                as: 'dondeDaClasesPorProfesor'
+            }
+        } , {$unwind: {
+            path: "$dondeDaClasesPorProfesor",
+            preserveNullAndEmptyArrays: false
+        }},
+        {$lookup: 
+            {
+                from: 'dondeDaClases',
+                localField: 'dondeDaClasesPorProfesor.idDondeDaClases_Fk',
+                foreignField: 'id_dondeClases',
+                as: 'dondeClases'
+            }
+        }
+    ]).exec((err, respuesta) => {
+        if (err) console.log("Error:"+ err);
+        console.log("Respuesta:" + respuesta);
+        return  respuesta;   
+    })
+};
+
+let getUsuarioByNameMail = (req, res) =>
+{      
+    //Listar resultados
+    var respuesta;
+    console.log("llegue a leer getUsuarioByNameMail "  + req.params.mail );
+    usuarios.aggregate([
+        {$match: { 'mail': { $regex:  req.params.mail , $options: 'i'}}},
+        {$lookup: 
+            {
+                from: 'direccion',
+                localField: 'idDireccion',
+                foreignField: 'idDireccion',
+                as: 'domicilio'
+            }
+        } ,
+        {$lookup: 
+            {
+                from: 'materiaPorProfesor',
+                localField: 'idUsuario',
+                foreignField: 'idProfesor',
+                as: 'materiaProfesores'
+            }
+        } , {$unwind: {
+            path: "$materiaProfesores",
+            preserveNullAndEmptyArrays: false
+        }}
+        ,{$lookup: 
+            {
+                from: 'materia',
+                localField: 'materiaProfesores.idMateria_FK',
+                foreignField: 'id_materia',
+                as: 'materias'
+            }
+        },
+        {$lookup: 
+            {
+                from: 'profesorPorClase',
+                localField: 'idUsuario',
+                foreignField: 'idProfesor',
+                as: 'clasesProfesores'
+            }
+        } , {$unwind: {
+            path: "$clasesProfesores",
+            preserveNullAndEmptyArrays: false
+        }},
+        {$lookup: 
+            {
+                from: 'tipoClase',
+                localField: 'clasesProfesores.idClase_FK',
+                foreignField: 'id_tipoClases',
+                as: 'tipoClases'
+            }
+        },
+        {$lookup: 
+            {
+                from: 'dondeDaClasesPorProfesor',
+                localField: 'idUsuario',
+                foreignField: 'idProfesor',
+                as: 'dondeDaClasesPorProfesor'
+            }
+        } , {$unwind: {
+            path: "$dondeDaClasesPorProfesor",
+            preserveNullAndEmptyArrays: false
+        }},
+        {$lookup: 
+            {
+                from: 'dondeDaClases',
+                localField: 'dondeDaClasesPorProfesor.idDondeDaClases_Fk',
+                foreignField: 'id_dondeClases',
+                as: 'dondeClases'
+            }
+        }
+    ]).exec((err, respuesta) => {
+        if (err) console.log(err);
+        console.log(JSON.stringify(respuesta));
+        res.send(respuesta);   
+    });
+};
+
+
+
+let getUsuarioByName = (req, res) =>
+{      
+    //Listar resultados
+    var respuesta;
+    console.log("llegue a leer getUsuarioByName + Nombre:"  + req.params.name );
+    usuarios.aggregate([
+        {$match: { 'nombre_usuario': { $regex:  req.params.name , $options: 'i'}}},
+        {$lookup: 
+            {
+                from: 'direccion',
+                localField: 'idDireccion',
+                foreignField: 'idDireccion',
+                as: 'domicilio'
+            }
+        } ,
+        {$lookup: 
+            {
+                from: 'materiaPorProfesor',
+                localField: 'idUsuario',
+                foreignField: 'idProfesor',
+                as: 'materiaProfesores'
+            }
+        } , {$unwind: {
+            path: "$materiaProfesores",
+            preserveNullAndEmptyArrays: false
+        }}
+        ,{$lookup: 
+            {
+                from: 'materia',
+                localField: 'materiaProfesores.idMateria_FK',
+                foreignField: 'id_materia',
+                as: 'materias'
+            }
+        },
+        {$lookup: 
+            {
+                from: 'profesorPorClase',
+                localField: 'idUsuario',
+                foreignField: 'idProfesor',
+                as: 'clasesProfesores'
+            }
+        } , {$unwind: {
+            path: "$clasesProfesores",
+            preserveNullAndEmptyArrays: false
+        }},
+        {$lookup: 
+            {
+                from: 'tipoClase',
+                localField: 'clasesProfesores.idClase_FK',
+                foreignField: 'id_tipoClases',
+                as: 'tipoClases'
+            }
+        },
+        {$lookup: 
+            {
+                from: 'dondeDaClasesPorProfesor',
+                localField: 'idUsuario',
+                foreignField: 'idProfesor',
+                as: 'dondeDaClasesPorProfesor'
+            }
+        } , {$unwind: {
+            path: "$dondeDaClasesPorProfesor",
+            preserveNullAndEmptyArrays: false
+        }},
+        {$lookup: 
+            {
+                from: 'dondeDaClases',
+                localField: 'dondeDaClasesPorProfesor.idDondeDaClases_Fk',
+                foreignField: 'id_dondeClases',
+                as: 'dondeClases'
+            }
+        }
+    ]).exec((err, respuesta) => {
+        if (err) console.log(err);
+        console.log(JSON.stringify(respuesta));
+        res.send(respuesta);   
+    });
+};
+
+module.exports = {getUsuarios,getUsuarioById,insertUsuario,updateUsuarioByPassword,
+    getUsuarioByIdOne,getUsuarioByMail,getUsuarioByNameMail,getUsuarioByName};
